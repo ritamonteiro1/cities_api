ce03245aec52aaf2714866d4345b3f584c77590b
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2023-06-21 17:08:13 -0300

    add yup

--- src/server/controllers/cities/create.ts
+++ src/server/controllers/cities/create.ts
@@ -1,12 +1,29 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 interface ICity {
     name: string;
 }
 
-export const create = (req: Request<{}, {}, ICity>, res: Response) => {
+const bodyValidation: yup.Schema<ICity> = yup.object().shape({
+    name: yup.string().required().min(3)
+});
 
-    console.log(req.body.name);
+export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
 
-    return res.send('create!');
+    let validateDate: ICity | undefined = undefined;
+
+    try {
+        validateDate = await bodyValidation.validate(req.body);
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        return res.json({
+            errors: {
+                default: yupError.message
+            }
+        });
+    }
+
+    console.log(validateDate);
 };
