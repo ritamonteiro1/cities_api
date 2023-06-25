4d79a31950be69d7d50e3021de3dadb8f07e487d
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2023-06-25 14:24:05 -0300

    add translations yup

--- src/server/controllers/cities/create.ts
+++ src/server/controllers/cities/create.ts
@@ -4,10 +4,12 @@ import * as yup from 'yup';
 
 interface ICity {
     name: string;
+    state: string;
 }
 
 const bodyValidation: yup.Schema<ICity> = yup.object().shape({
-    name: yup.string().required().min(3)
+    name: yup.string().required().min(3),
+    state: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
@@ -15,14 +17,17 @@ export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
     let validateDate: ICity | undefined = undefined;
 
     try {
-        validateDate = await bodyValidation.validate(req.body);
+        validateDate = await bodyValidation.validate(req.body, { abortEarly: false });
     } catch (error) {
         const yupError = error as yup.ValidationError;
-        return res.json({
-            errors: {
-                default: yupError.message
-            }
+        const errors: Record<string, string> = {};
+
+        yupError.inner.forEach(error => {
+            if (error.path === undefined) return;
+            errors[error.path] = error.message;
         });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({errors});
     }
 
     console.log(validateDate);
