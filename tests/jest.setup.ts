3f6a6760ecc9dd145d028e4b87b6d40757743569
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2023-09-07 16:05:38 -0300

    update jest.setup.ts

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,6 +1,14 @@
 import supertest from 'supertest';
 
 import { server } from '../src/server/server';
+import { Knex } from '../src/server/database/knex';
 
+beforeAll(async () => {
+    await Knex.migrate.latest();
+});
+
+afterAll(async () => {
+    await Knex.destroy();
+});
 
 export const testServer = supertest(server);
