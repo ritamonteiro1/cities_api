6b8faa50963d8833f5df916e0ee94ed766ab58be
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2023-09-07 15:56:42 -0300

    update controller city

--- src/server/controllers/cities/create.ts
+++ src/server/controllers/cities/create.ts
@@ -10,7 +10,7 @@ interface IBodyProps extends Omit<ICity, 'id'> {}
 export const createValidation = validation((getSchema) => ({
     body: getSchema<IBodyProps>(
         yup.object().shape({
-            name: yup.string().required().min(3),
+            name: yup.string().required().min(3).max(150),
         })
     ),
 }));
