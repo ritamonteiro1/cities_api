bca4c00550d0a5785f99fd0b9a1c4d63db35d9e3
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2023-09-08 15:50:34 -0300

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/server';
 
-server.listen(process.env.PORT || 3333, () => {
-    console.log(`Server is running ${process.env.PORT || 3333}`);
-});
+const startServer = () => {
+    server.listen(process.env.PORT || 3333, () => {
+        console.log(`Server is running: localhost ${process.env.PORT || 3333}`);
+    });
+};
 
 
+if (process.env.IS_LOCALHOSTAL !== 'true') {
+    Knex.migrate.latest().then(() => {
+        startServer();
+    }).catch((err) => {
+        console.log(err);
+    });
+} else {
+    startServer();
+}
